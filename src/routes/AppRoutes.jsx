5fdbc25560a040d945ea0d8509a75d6f1c756e3a
Ajai28 <ajaidharnish38@gmail.com>
2025-05-31 14:34:29 +0530

    Routes-update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import { Route,Routes } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import Home from '../pages/Home';
+import PrivateRoute from '../Routes/PrivateRoute';
+
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/Login" element={<Login />} />
       <Route path="/Signup" element={<Signup />} />
+      <Route
+        path="/Home"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
       <Route path="*" element={<Login />} />
-      <Route path="/Home" element={<Home />} />
     </Routes>
-  )
+  );
 }
